6a9ee9a6ce655bd0e14201b349956b11e279ff9d
root <root@ce-front0.dev.kaltura.com>
2015-03-15 00:20:38 -0400

    New clientlib ver

--- ruby/lib/kaltura_client_base.rb
+++ ruby/lib/kaltura_client_base.rb
@@ -368,7 +368,7 @@ module Kaltura
 		def initialize(partner_id = -1,service_url="http://www.kaltura.com")
 			@service_url 	= service_url
 			@format 		= 2 # xml
-			@client_tag 	= "ruby:15-03-14"
+			@client_tag 	= "ruby:15-03-15"
 			@timeout 		= 120
 			@partner_id 	= partner_id
 			@requestHeaders = {}

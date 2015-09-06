9b1beeaec5e8b9cbb8d975a4f1610d1f131bc8b0
Kaltura CSI <community@kaltura.com>
2015-09-06 01:21:10 -0400

    New clientlib ver

--- lib/kaltura_client_base.rb
+++ lib/kaltura_client_base.rb
@@ -368,7 +368,7 @@ module Kaltura
 		def initialize(partner_id = -1,service_url="http://www.kaltura.com")
 			@service_url 	= service_url
 			@format 		= 2 # xml
-			@client_tag 	= "ruby:15-09-05"
+			@client_tag 	= "ruby:15-09-06"
 			@timeout 		= 120
 			@partner_id 	= partner_id
 			@requestHeaders = {}

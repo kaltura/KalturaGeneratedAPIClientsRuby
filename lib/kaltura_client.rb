ac698a9206c4d18f1ccde6fbe662c83d0f34b385
Kaltura CSI <community@kaltura.com>
2020-01-22 01:18:37 -0500

    New clientlib ver

--- lib/kaltura_client.rb
+++ lib/kaltura_client.rb
@@ -6230,7 +6230,7 @@ module Kaltura
 		
 		def initialize(client)
 			super(client)
-			self.client_tag = 'ruby:20-01-21'
+			self.client_tag = 'ruby:20-01-22'
 			self.api_version = '15.15.0'
 		end
 		
